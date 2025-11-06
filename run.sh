864e7f6e4d55bbb4bbd32522633cd9a741d42cb7
Jan Friedrich <freeandnil@apache.org>
2025-11-06 21:03:01 +0100

    remove $ICA_ROOT_HOST - not working on my Mint install

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 set -x
 
-ICA_ROOT_HOST="$HOME/.config/citrix-receiver-container/root/.ICAClient"
-mkdir -p $ICA_ROOT_HOST
-
 # cat /proc/sys/net/ipv4/ip_local_port_range
 PULSE_AUDIO_BRIDGE_PORT="53971"
 LANG=c PULSE_AUDIO_BRIDGE_INDEX="$(pactl load-module module-native-protocol-tcp  port=$PULSE_AUDIO_BRIDGE_PORT auth-ip-acl=127.0.0.1)"
@@ -18,7 +15,6 @@ podman run --rm \
   -e TZ="Europe/Berlin" \
   --net=host \
   -e PULSE_SERVER=tcp:127.0.01:$PULSE_AUDIO_BRIDGE_PORT \
-  -v $ICA_ROOT_HOST:/root/.ICAClient \
   citrix
 
 pactl unload-module $PULSE_AUDIO_BRIDGE_INDEX
