5a1b75d8e34a10b12d3c6a7f596e976d2e6ce2e0
Lukas Gelke <Lukas.Gelke@komsa.de>
2025-11-06 12:30:36 +0100

    make pulse audio server optional/configurable

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
+#!/bin/bash -x
 set -x
 
 ICA_ROOT_HOST="$HOME/.config/citrix-receiver-container/root/.ICAClient"
 mkdir -p $ICA_ROOT_HOST
 
-# cat /proc/sys/net/ipv4/ip_local_port_range
-PULSE_AUDIO_BRIDGE_PORT="53971"
-LANG=c PULSE_AUDIO_BRIDGE_INDEX="$(pactl load-module module-native-protocol-tcp  port=$PULSE_AUDIO_BRIDGE_PORT auth-ip-acl=127.0.0.1)"
+PULSE_AUDIO_BRIDGE_INDEX=""
+PULSE_SERVER_FOR_DOCKER="${PULSE_SERVER_FOR_DOCKER:-$PULSE_SERVER}"
+if [[ -z "${PULSE_SERVER_FOR_DOCKER}" ]]; then
+  # cat /proc/sys/net/ipv4/ip_local_port_range
+  PULSE_AUDIO_BRIDGE_PORT="${PULSE_AUDIO_BRIDGE_PORT:-53971}"
+  LANG=c PULSE_AUDIO_BRIDGE_INDEX="$(pactl load-module module-native-protocol-tcp port=$PULSE_AUDIO_BRIDGE_PORT auth-ip-acl=127.0.0.1)"
+  PULSE_SERVER_FOR_DOCKER="tcp:127.0.0.1:$PULSE_AUDIO_BRIDGE_PORT"
+fi
 
 podman run --rm \
   -e DISPLAY=$DISPLAY \
@@ -17,9 +23,11 @@ podman run --rm \
   --name citrix \
   -e TZ="Europe/Berlin" \
   --net=host \
-  -e PULSE_SERVER=tcp:127.0.01:$PULSE_AUDIO_BRIDGE_PORT \
+  -e PULSE_SERVER=$PULSE_SERVER_FOR_DOCKER \
   -e USE_FIREFOX_FOR_SELFSERVICE \
   -v $ICA_ROOT_HOST:/root/.ICAClient \
   citrix "$@"
 
-pactl unload-module $PULSE_AUDIO_BRIDGE_INDEX
+if ! [[ -z "${PULSE_AUDIO_BRIDGE_INDEX}" ]]; then
+  pactl unload-module $PULSE_AUDIO_BRIDGE_INDEX
+fi
